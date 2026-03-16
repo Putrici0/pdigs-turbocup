b7be131a0cced0f5a28242507671b83b8d2966c4
002avid <david6espino@gmail.com>
2026-03-16 16:31:08 +0000

    Get all tournamnts draft

--- backend/app/api/tournaments.py
+++ backend/app/api/tournaments.py
@@ -1,11 +1,43 @@
-from flask import Blueprint, jsonify
+from datetime import datetime
 
-tournaments_bp = Blueprint('tournaments', __name__)
+from firebase_admin import credentials
+from flask import Blueprint, jsonify, Flask
+from firebase_admin import firestore
+import firebase_admin
+import json
 
-@tournaments_bp.route('/', methods=['GET'])
+
+
+tournaments_bp = Flask(__name__)
+
+if not firebase_admin._apps:
+    cred = credentials.Certificate("../../firebase-credentials.json")
+    firebase_admin.initialize_app(cred)
+
+db = firestore.client()
+
+def serialize_firestore(doc):
+    item = doc.to_dict()
+
+    for key, value in item.items():
+        if isinstance(value, datetime):
+            item[key] = value.isoformat()
+
+    item["id"] = doc.id
+    return item
+
+
+@tournaments_bp.route("/tournaments", methods=["GET"])
 def get_tournaments():
-    return jsonify({"message": "GET tournaments working"}), 200
+    tournaments = db.collection("tournaments").stream()
+    data = [serialize_firestore(doc) for doc in tournaments]
+
+    return jsonify(data), 200
+
 
 @tournaments_bp.route('/', methods=['POST'])
 def create_tournament():
     return jsonify({"message": "POST tournament working"}), 201
+
+if __name__ == "__main__":
+    tournaments_bp.run(debug=True)
