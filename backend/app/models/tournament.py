3e3334767110cd7f56205d9770d61c90b486fa19
Doffensmirthz <drdoffensmirthz@gmail.com>
2026-03-16 23:32:19 +0000

    Added post tournament api and deleted points in model tournament

--- backend/app/models/tournament.py
+++ backend/app/models/tournament.py
@@ -5,15 +5,11 @@ class Tournament:
         self.end_date = end_date # End date and time
         self.status = status # "scheduled", "current", "past"
 
-        # Diccionario para guardar los puntos logrados por cada participante
-        # Formato esperado: {"team_id_1": 15, "team_id_2": 10}
-        self.participants_points = {}
 
     def to_dict(self):
         return {
             "name": self.name,
             "start_date": self.start_date,
             "end_date": self.end_date,
-            "status": self.status,
-            "participants_points": self.participants_points
+            "status": self.status
         }
