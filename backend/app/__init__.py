99e423016e29c42b42767aebc07107f5d5e24c25
002avid <adbugyf@gmail.com>
2026-03-18 19:37:40 +0000

    Connected frontend and backend for the general view of the tournaments.

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,22 +1,17 @@
 from flask import Flask
-
-from backend.app.api.matches import matches_bp
-from backend.app.api.stats import stats_bp
-from backend.app.api.teams import teams_bp
+from flask_cors import CORS  # 1. Importamos la librería nueva
 from backend.app.api.tournaments import tournaments_bp
-
 
 def create_app():
     app = Flask(__name__)
 
-    @app.route('/health', methods=['GET'])
-    def health_check():
-        return {"status": "ok", "message": "API scaffolding successful for TurboCup"}
+    # 2. Activamos CORS para que el navegador confíe en nuestra API
+    CORS(app)
 
     app.register_blueprint(tournaments_bp, url_prefix='/api/tournaments')
-    app.register_blueprint(teams_bp, url_prefix='/api/teams')
-    app.register_blueprint(matches_bp, url_prefix='/api/matches')
-    app.register_blueprint(stats_bp, url_prefix='/api/stats')
+    #app.register_blueprint(teams_bp, url_prefix='/api/teams')
+    #app.register_blueprint(matches_bp, url_prefix='/api/matches')
+    #app.register_blueprint(stats_bp, url_prefix='/api/stats')
 
 
     return app
