9556c3e120446cd76d13a4feb4e653e816077a3d
002avid <david6espino@gmail.com>
2026-04-13 16:50:40 +0100

    Handled password in user model.

--- backend/app/models/user.py
+++ backend/app/models/user.py
@@ -2,13 +2,12 @@ from backend.app.models.user_role import user_role
 
 
 class User:
-    def __init__(self, uid, name, surname, username, email, password, role: user_role):
+    def __init__(self, uid, name, surname, username, email, password, role):
         self.uid = uid
         self.name = name
         self.surname = surname
         self.username = username
         self.email = email
-        self.password = password
         self.role = role
 
     def to_dict(self):
@@ -18,6 +17,5 @@ class User:
             "surname": self.surname,
             "username": self.username,
             "email": self.email,
-            "password": self.password,
-            "role": self.role
+            "role": self.role.value
         }
