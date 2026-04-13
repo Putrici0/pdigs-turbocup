be3e38c84f66f3d65c1bcb217a57d02808a3da3b
paull <paullopisparrilla@gmail.com>
2026-04-13 17:14:29 +0100

    added users code using firebase pt2 (added the API public key)

--- frontend-angular/src/app/core/firebase.ts
+++ frontend-angular/src/app/core/firebase.ts
@@ -3,12 +3,12 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: 'TU_API_KEY',
-  authDomain: 'TU_PROYECTO.firebaseapp.com',
-  projectId: 'TU_PROJECT_ID',
-  storageBucket: 'TU_PROYECTO.firebasestorage.app',
-  messagingSenderId: 'TU_MESSAGING_SENDER_ID',
-  appId: 'TU_APP_ID',
+  apiKey: "AIzaSyC6yWzFAhInaVwYKXSKla2rna1UK2Rnwwk",
+  authDomain: "turbocup-77c28.firebaseapp.com",
+  projectId: "turbocup-77c28",
+  storageBucket: "turbocup-77c28.firebasestorage.app",
+  messagingSenderId: "749477364801",
+  appId: "1:749477364801:web:a6f85d6d6c42ac56bd32c0"
 };
 
 const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
