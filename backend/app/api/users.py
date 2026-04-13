862e86b13aa25381d9d78fe509b3008ee16faed2
Joan <drdoffensmirhtz@gmail.com>
2026-04-13 16:20:44 +0100

    import request, change date

--- backend/app/api/users.py
+++ backend/app/api/users.py
@@ -1,9 +1,12 @@
 from firebase_admin import auth, firestore
+from datetime import datetime
+
 from flask import Blueprint, jsonify, request
 
 from backend.app.db import db
 
 users_bp = Blueprint("user", __name__)
+
 @users_bp.route('/', methods=['POST'])
 def create_user():
     data = request.json
@@ -12,23 +15,35 @@ def create_user():
     password = data.get("password")
 
     if not email or not password:
-        return jsonify({"error":"Missing email or password"}), 400
+        return jsonify({"error": "Missing email or password"}), 400
 
-    user = auth.create_user(
-        email=email,
-        password=password
-    )
+    try:
+        user = auth.create_user(
+            email=email,
+            password=password
+        )
 
-    uid = user.uid
+        uid = user.uid
 
-    db.collection("users").document(uid).set({
-        "email": email,
-        "stats": {
-            "matchesPlayed": 0,
-            "wins": 0,
-        },
-        "createdAt": firestore.SERVER_TIMESTAMP
-    })
+        db.collection("users").document(uid).set({
+            "email": email,
+            "stats": {
+                "matchesPlayed": 0,
+                "wins": 0,
+            },
+            "createdAt": datetime.utcnow()
+        })
+
+        return jsonify({
+            "message": "User created",
+            "uid": uid
+        }), 201
+
+    except auth.EmailAlreadyExistsError:
+        return jsonify({"error": "Email already in use"}), 409
+
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
 
 @users_bp.route('/<user_id>', methods=['GET'])
 def get_user(user_id):
